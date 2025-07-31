43eea536480405ccfbfe62637d2f8e4c796b02c8
strzcy <desipuspita46832@gmail.com>
2025-07-31 19:00:06 +0700

    migrate loh

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="alert alert-warning" role="alert">
+        a simple warning alert check it out
+    </div>
+@endsection
--- resources/views/layouts/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.app')
-
-@section('content')
-<div class="alert alert-warning" role="alert">
-    asimple warning alert check it out
-</div>
-@endsection
